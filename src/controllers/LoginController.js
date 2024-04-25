bc26b22c780f4fd76d780bdc854bec61826ba01b
DanielBieer <danibierdasilva@gmail.com>
2024-04-24 21:54:18 -0300

    adiciona-criptografia no Aluno

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,3 +1,4 @@
+const { compare } = require("bcryptjs")
 const Aluno = require("../models/Aluno")
 const { sign } = require('jsonwebtoken')
 
@@ -16,15 +17,21 @@ class LoginController{
             if (!password) {
                 return res.status(400).json({ message: 'O password é obrigatório' })
             }
-    
-            const aluno = await Alunouno.findOne({
-                where: {email:email, password:password}
+           
+
+            const aluno = await Aluno.findOne({
+                where: {email:email }
             })
-    
-            if(!aluno){
+
+             if(!aluno){
                 return res.status(404).json({ error: 'Nenhum aluno corresponde a email e senha fornecidos!' })
             }
-    
+
+            const hashSenha = await compare(password, aluno.password)
+             
+              if(hashSenha === false){
+                return res.status(403).json({message: 'Aluno não encontrado'})
+            }  
             const payload = {sub: aluno.id, email: aluno.email, nome: aluno.nome}
     
             const token = sign(payload, process.env.SECRET_JWT)        
